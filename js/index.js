6705a2b0797bd08e6766a29f39f390e5d5ff4cab
Nachoofiore <Nachoofiore@hotmail.com>
2025-08-30 20:45:02 -0300

    Funcional 2

--- js/index.js
+++ js/index.js
@@ -3,18 +3,17 @@ document.addEventListener("DOMContentLoaded", function(){
   const profileDropdownToggle = document.getElementById("profileDropdown");
   const storedUsername = localStorage.getItem("username");
 
-  // Verificar si el usuario ha iniciado sesión
+   if (storedUsername) {
+    profileDropdownToggle.textContent = storedUsername;
+  }
+
+    // Verificar si el usuario ha iniciado sesión
   if (!storedUsername) {
     // Redirigir al login con mensaje
     localStorage.setItem("loginMessage", "Por favor inicie sesión");
     window.location.href = "login.html";
     return; // Detener la ejecución del resto del código
   }
-
-  if (storedUsername) {
-    profileDropdownToggle.textContent = storedUsername;
-  }
-  
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
