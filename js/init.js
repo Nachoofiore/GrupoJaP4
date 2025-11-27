2e454ec9fadfef111f387aa6476e10e581fbe1c3
Lucía Prestes <lupre77@gmail.com>
2025-11-26 21:30:03 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 constante  CATEGORÍAS_URL  =  "https://japceibal.github.io/emercado-api/cats/cat.json" ;
 const  PUBLISH_PRODUCT_URL  =  "https://japceibal.github.io/emercado-api/sell/publish.json" ;
 const  PRODUCTOS_URL  =  "https://japceibal.github.io/emercado-api/cats_products/" ;
@@ -6,6 +7,16 @@ const  PRODUCT_INFO_COMMENTS_URL  =  "https://japceibal.github.io/emercado-api/p
 const  CART_INFO_URL  =  "https://japceibal.github.io/emercado-api/user_cart/" ;
 const  CART_BUY_URL  =  "https://japceibal.github.io/emercado-api/cart/buy.json" ;
 constante  EXT_TYPE  =  ".json" ;
+=======
+const CATEGORIES_URL = "http://localhost:3000/cats/cat";
+const PUBLISH_PRODUCT_URL = "http://localhost:3000/sell/publish";
+const PRODUCTS_URL = "http://localhost:3000/cats_products";
+const PRODUCT_INFO_URL = "http://localhost:3000/products";
+const PRODUCT_INFO_COMMENTS_URL = "http://localhost:3000/products_comments";
+const CART_INFO_URL = "http://localhost:3000/user_cart";
+const CART_BUY_URL = "http://localhost:3000/cart/buy";
+
+>>>>>>> Stashed changes
 
 
 let hideSpinner = function(){
